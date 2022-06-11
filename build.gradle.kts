53d911cbc871c537b99f758252c223780fbe493e
xugang <chuangcius@gmail.com>
2022-06-11 19:48:52 +0800

    update jdk to 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,8 +25,8 @@ group = "com.chuangcius"
 version = "1.0.0-SNAPSHOT"
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_11
-    targetCompatibility = JavaVersion.VERSION_11
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 tasks.withType<JavaCompile> {
